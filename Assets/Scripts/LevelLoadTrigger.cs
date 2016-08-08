ba140d24f6db6d49aafbdd1071654b74febcf0d4
hacktavist <hacktavist616@gmail.com>
2016-08-08 15:57:53 -0400

    lot of level building; minor script tweaks

--- Assets/Scripts/LevelLoadTrigger.cs
+++ Assets/Scripts/LevelLoadTrigger.cs
@@ -2,18 +2,18 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
-using UnityStandardAssets.ImageEffects;
 public class LevelLoadTrigger : MonoBehaviour {
   public GameObject cam;
+  public float smooth = 1f;
   public string keyToPress;
   public string levelToLoad;
   public Text buttonPressInteract;
   public Image buttonToPress;
-  Bloom camEffect;
   bool toggle;
+  public Light camEffect;
   // Use this for initialization
   void Awake() {
-    camEffect = cam.GetComponent<Bloom>();
+    
     buttonToPress.sprite = Resources.Load("keys/" + keyToPress, typeof(Sprite)) as Sprite;
   }
 
@@ -29,8 +29,9 @@ public class LevelLoadTrigger : MonoBehaviour {
     }
   }
   void OnTriggerStay(Collider c) {
-    if (c.tag == "Player" && Input.GetKeyDown(keyToPress))
+    if (c.tag == "Player" && Input.GetKeyDown(keyToPress)) {
       StartCoroutine(CameraEffects(levelToLoad));
+    }
   }
   void OnTriggerExit(Collider c) {
     if (c.tag == "Player") {
@@ -40,12 +41,14 @@ public class LevelLoadTrigger : MonoBehaviour {
   }
 
   IEnumerator CameraEffects(string level) {
-    camEffect.bloomIntensity = -4f;
+    for (int i = 0; i < 20; i++) {
+      camEffect.intensity = i;
+    }
     AsyncOperation async = SceneManager.LoadSceneAsync(level);
     while (!async.isDone) {
       
-      if(camEffect.bloomIntensity <=4 && camEffect.bloomIntensity >=-4)
-      camEffect.bloomIntensity = async.progress * 100/90;
+      //if(camEffect.bloomIntensity <=4 && camEffect.bloomIntensity >=-4)
+      camEffect.intensity -= async.progress * 100;
       yield return null;
     }
   }
