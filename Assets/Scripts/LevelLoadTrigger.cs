e7af01124a54cf318dde2163c95866d230b81a14
hacktavist <hacktavist616@gmail.com>
2016-08-08 11:52:16 -0400

    added image component to levelload interact; can change the keystroke in editor now; image synced with that variable

--- Assets/Scripts/LevelLoadTrigger.cs
+++ Assets/Scripts/LevelLoadTrigger.cs
@@ -5,13 +5,16 @@ using System.Collections;
 using UnityStandardAssets.ImageEffects;
 public class LevelLoadTrigger : MonoBehaviour {
   public GameObject cam;
+  public string keyToPress;
   public string levelToLoad;
   public Text buttonPressInteract;
+  public Image buttonToPress;
   Bloom camEffect;
   bool toggle;
   // Use this for initialization
   void Awake() {
     camEffect = cam.GetComponent<Bloom>();
+    buttonToPress.sprite = Resources.Load("keys/" + keyToPress, typeof(Sprite)) as Sprite;
   }
 
   // Update is called once per frame
@@ -20,17 +23,20 @@ public class LevelLoadTrigger : MonoBehaviour {
   }
 
   void OnTriggerEnter(Collider c) {
-    if (c.tag == "Player") 
-      buttonPressInteract.text = "Press E to Interact";
-    
+    if (c.tag == "Player") {
+      buttonPressInteract.text = "Press";
+      buttonToPress.enabled = true;
+    }
   }
   void OnTriggerStay(Collider c) {
-    if (c.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+    if (c.tag == "Player" && Input.GetKeyDown(keyToPress))
       StartCoroutine(CameraEffects(levelToLoad));
   }
   void OnTriggerExit(Collider c) {
-    if (c.tag == "Player")
+    if (c.tag == "Player") {
       buttonPressInteract.text = "";
+      buttonToPress.enabled = false;
+    }
   }
 
   IEnumerator CameraEffects(string level) {
--- Assets/Scripts/QTETrigger.cs
+++ Assets/Scripts/QTETrigger.cs
@@ -3,86 +3,75 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class QTETrigger : MonoBehaviour
-{
-    public enum QTEState { Ready, Ongoing, Done };
-    public QTEState state = QTEState.Ready;
-    public enum QTEResponse { Null, Success, Fail };
-    public QTEResponse response = QTEResponse.Null;
+public class QTETrigger : MonoBehaviour {
+  public enum QTEState { Ready, Ongoing, Done };
+  public QTEState state = QTEState.Ready;
+  public enum QTEResponse { Null, Success, Fail };
+  public QTEResponse response = QTEResponse.Null;
 
-    public List<string> Buttons = new List<string>();
-    public List<string> CopyButtons;
-    public Image buttonDisplay;
-    public float delay = 1f;
+  public List<string> Buttons = new List<string>();
+  public List<string> CopyButtons;
+  public Image buttonDisplay;
 
-    private int randomNumber = 0;
-    private float LastButtonPressed = 0f;
 
-    void Awake() {
-      CopyButtons = new List<string>(Buttons);
+  private int randomNumber = 0;
+  private float LastButtonPressed = 0f;
+
+  void Awake() {
+    CopyButtons = new List<string>(Buttons);
+  }
+
+  void OnTriggerEnter(Collider c) {
+    if (state == QTEState.Ready && c.tag == "Player") {
+      // Modded FirstPersonController to have a static variable to freeze the script
+      FirstPersonController.pause();
+      PickRandomButton();
+
     }
+  }
 
-    void OnTriggerEnter(Collider c)
-    {
-        if (state == QTEState.Ready && c.tag == "Player")
-        {
-            // Modded FirstPersonController to have a static variable to freeze the script
-            FirstPersonController.pause();
-            PickRandomButton();
-            
+
+  void Update() {
+    if (state == QTEState.Ongoing && Input.anyKeyDown) {
+      if (Input.GetKeyDown(Buttons[randomNumber])) {
+        state = QTEState.Done;
+        response = QTEResponse.Success;
+        print("correct!");
+        Buttons.RemoveAt(randomNumber);
+        if (Buttons.Count == 0) {
+          state = QTEState.Done;
+          print("woohoo your done!");
+          buttonDisplay.enabled = false;
+          FirstPersonController.unpause();
+          Destroy(gameObject);
+        } else {
+          PickRandomButton();
         }
-    }
-    
-
-    void Update()
-    {
-        if (state == QTEState.Ongoing && Input.anyKeyDown)
-        {
-            if (Input.GetKeyDown(Buttons[randomNumber]))
-            {
-                state = QTEState.Done;
-                response = QTEResponse.Success;
-                print("correct!");
-                Buttons.RemoveAt(randomNumber);
-                if (Buttons.Count == 0)
-                {
-                    state = QTEState.Done;
-                    print("woohoo your done!");
-                    buttonDisplay.enabled = false;
-                    FirstPersonController.unpause();
-                    Destroy(gameObject);
-                }
-                else
-                {
-                    PickRandomButton();
-                }
-            }
-            else
-            {
-              state = QTEState.Ready;
-              response = QTEResponse.Null;
-              FirstPersonController.unpause();
-              buttonDisplay.enabled = false;
-              Buttons.Clear();
-              Buttons = new List<string>(CopyButtons);
-                print("wrong!");
-            }
-        }
+      } else {
+        state = QTEState.Ready;
+        response = QTEResponse.Null;
+        FirstPersonController.unpause();
+        buttonDisplay.enabled = false;
+        Buttons.Clear();
+        Buttons = new List<string>(CopyButtons);
+        print("wrong!");
+      }
     }
 
-    private void PickRandomButton()
-    {
-        int count = Buttons.Count;
-        buttonDisplay.enabled = true;
-        LastButtonPressed = Time.time;
-        if (count > 0)
-        {
-            state = QTEState.Ongoing;
-            randomNumber = Random.Range(0, Buttons.Count);
+  }
 
-            // dynamically load button sprite by name, idea is to avoid hard coding as much as possible
-            buttonDisplay.sprite = Resources.Load("keys/" + Buttons[randomNumber], typeof(Sprite)) as Sprite;
-        }
+  private void PickRandomButton() {
+    int count = Buttons.Count;
+    buttonDisplay.enabled = true;
+    LastButtonPressed = Time.time;
+    if (count > 0) {
+      state = QTEState.Ongoing;
+      randomNumber = Random.Range(0, Buttons.Count);
+
+      // dynamically load button sprite by name, idea is to avoid hard coding as much as possible
+      buttonDisplay.sprite = Resources.Load("keys/" + Buttons[randomNumber], typeof(Sprite)) as Sprite;
     }
-    
+
+  }
+
 }
