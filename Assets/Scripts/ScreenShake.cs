1f92f8614b19123589df82ed63ae7626d4b087c1
jwang1919 <jesse.wang19@gmail.com>
2016-08-14 15:07:33 -0400

    Added more sounds. Scripts to make sandwich shop playable. Added music suggested by Luciana.

---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShake : MonoBehaviour {
+
+    public float shakeAmount = 0.7F;
+    public float decreaseFactor = 1F;
+    private bool flag = false;
+
+    private float initialShakeAmount;
+    private float initialDecreaseFactor;
+    private float initialShake;
+
+	// Use this for initialization
+	void Start () {
+        initialShakeAmount = shakeAmount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.P)) {
+            flag = true;
+        }
+
+        if (flag) {
+            if (shakeAmount > 0F) {
+                float currentY = Camera.main.transform.localPosition.y;
+                Camera.main.transform.localPosition = new Vector3(Random.value * shakeAmount, currentY, Random.value * shakeAmount);
+                shakeAmount -= Time.deltaTime * decreaseFactor;
+            }
+            else {
+                flag = false;
+                shakeAmount = initialShakeAmount;
+            }
+        }
+    }
+}
