4fe1bf0b82b2d7006abdf5b1bcbcecdc0489c76d
jwang1919 <jesse.wang19@gmail.com>
2016-08-14 23:19:01 -0400

    Merge remote-tracking branch 'upstream/sandwichshopandsilomerged' into sandwichshopandsilomerged
    # Conflicts:
    #	Assets/Scenes/SiloScene.unity

---
+++ Assets/Scripts/Silo/DealWithText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+public class DealWithText : MonoBehaviour
+{
+
+    Text flav;
+    bool flavIsOn;
+    // Use this for initialization
+    void Start()
+    {
+        FirstPersonController.pause();
+        flav = GameObject.Find("FlavorText").GetComponent<Text>();
+        flavIsOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flavIsOn)
+        {
+
+            RidText();
+        }
+    }
+
+    void RidText()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            flav.enabled = false;
+            flavIsOn = !flavIsOn;
+            FirstPersonController.unpause();
+        }
+    }
+}
