7f0a2ce7b492296c874616e94c20104b18c2ea57
jwang1919 <jesse.wang19@gmail.com>
2016-08-16 23:44:33 -0400

    Added room to test playing videos in Unity.

---
+++ Assets/Scripts/PodcastRoom/StartProjector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class StartProjector : MonoBehaviour {
+
+    public MovieTexture movie;
+
+    private GameObject screen;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        screen = GameObject.FindGameObjectWithTag("Screen");
+        if (screen == null)
+        {
+            throw new SystemException("Screen not found");
+        }
+
+        screen.GetComponent<Renderer>().material.mainTexture = movie;
+        audioSource = screen.GetComponent<AudioSource>();
+        audioSource.clip = movie.audioClip;
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        if (c.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !movie.isPlaying)
+        {
+            Debug.Log("Playing videos");
+            audioSource.Play();
+            movie.Play();
+        }
+    }
+}
