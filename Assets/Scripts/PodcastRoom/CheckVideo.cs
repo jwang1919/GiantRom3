258da69efea4d4ea7146677d0a70c3b75e9a77bf
hacktavist <hacktavist616@gmail.com>
2016-08-18 01:46:15 -0400

    made folder structure for scripts:

---
+++ Assets/Scripts/PodcastRoom/CheckVideo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CheckVideo : MonoBehaviour {
+
+  StartProjector movie;
+  Image load;
+	// Use this for initialization
+	void Start () {
+    movie = gameObject.GetComponentInChildren<StartProjector>();
+    load = GameObject.Find("Load").GetComponent<Image>();
+    load.CrossFadeAlpha(0f, 0f, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (movie.playing) {
+      StartCoroutine(WaitingForMovie(movie.movie.duration, OnWaitFinish));
+    }
+	}
+  void OnWaitFinish() {
+    load.CrossFadeAlpha(1f, 3f, true);
+  }
+
+  IEnumerator WaitingForMovie(float duration, System.Action callback){
+    while (movie.movie.isPlaying) {
+      yield return 0;
+    }
+    if (callback != null) callback();
+    yield break;
+  }
+}
